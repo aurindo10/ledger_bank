ff0fcd36735e12f626095accec40b3e1e617ba16
Aurindo Neto <aurindo15@gmail.com>
2024-05-13 22:38:04 -0300

    chore: Update handlerConsumeSqs.go and db.go

--- internal/routes/transactionRoute/handlerConsumeSqs.go
+++ internal/routes/transactionRoute/handlerConsumeSqs.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"ledger_bank/internal/domain/account"
 	"ledger_bank/internal/repositories"
+	"ledger_bank/internal/utils"
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func HandlerConsumeSqs(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
 		msg := message.MessageAttributes
-
 		fmt.Println("Mensagem:", msg)
 		value := msg["value"]
 		id := msg["id"]
@@ -32,8 +33,24 @@ func HandlerConsumeSqs(ctx context.Context, sqsEvent events.SQSEvent) error {
 		domain := account.NewUpdateTransaction(repo)
 		err = domain.Execute(&request)
 		if err != nil {
+			return err
+		}
+		queue := "ledger"
+		result, err := utils.SqsConnection.GetQueueUrl(&sqs.GetQueueUrlInput{
+			QueueName: &queue,
+		})
+		if err != nil {
 			println(err.Error(), "erro 37")
 		}
+		receiptHandle := message.ReceiptHandle
+		utils.SqsConnection.DeleteMessage(&sqs.DeleteMessageInput{
+			QueueUrl:      result.QueueUrl,
+			ReceiptHandle: &receiptHandle,
+		})
+		if err != nil {
+			println(err.Error(), "erro 50")
+		}
+		println("item da fila deletado com sucesso!")
 	}
 	return nil
 }
--- internal/utils/db.go
+++ internal/utils/db.go
@@ -3,13 +3,16 @@ package utils
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 var DataBaseClient *dynamodb.DynamoDB
+var SqsConnection *sqs.SQS
 
 func NewClientDb() {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
+	SqsConnection = sqs.New(sess)
 	DataBaseClient = dynamodb.New(sess)
 }
